d765542a206e990ac02a5b3660f2d835a1b1ff93
Valentijn.vw <33581307+Valentijnvw@users.noreply.github.com>
2020-12-17 15:02:36 +0100

    "Misschien vindt je dit ook leuk" tabel toevoegen

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -13,4 +13,11 @@ CREATE TABLE categories (
   ID SERIAL PRIMARY KEY,
   name VARCHAR(255),
   description TEXT
-); 
+);
+
+DROP TABLE IF EXISTS related_products;
+CREATE TABLE related_products (
+  ID SERIAL PRIMARY KEY,
+  product1_id INTEGER,
+  product2_id INTEGER
+);
